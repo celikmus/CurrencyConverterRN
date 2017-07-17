7a6d931ed4ab0d340dee805ab9dcdef10122cfb3
Mustafa Çelik <celikmus@gmail.com>
2017-07-17 11:49:55 +0100

    ListItem.

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4f6d7a',
   $white: '#ffffff',
   $border: '#e2e2e2',
   $inputText: '#797979',
-  $lightGray: '#f0f0f0'
+  $lightGray: '#f0f0f0',
+  $darkText: '#343434'
 
   // outline: 1
 });
-export default () => <Home />;
+export default () => <CurrencyList />;
