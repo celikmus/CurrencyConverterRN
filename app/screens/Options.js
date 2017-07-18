646a4a2792fe1b0475e86a4caf682d20d138ff3f
Mustafa Çelik <celikmus@gmail.com>
2017-07-18 12:46:08 +0100

    Linking external URL.

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
 
@@ -16,7 +16,7 @@ class Options extends Component {
     this.props.navigation.navigate('Themes');
   };
   handlePressSite = () => {
-    console.log('press site');
+    Linking.openURL('http://fixer.io').catch(() => alert('An error occured'));
   };
   render() {
     return (
